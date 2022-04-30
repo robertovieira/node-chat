f67e96ef94f101d8e67f42ff238b26d4515146d3
Roberto Vieira <roberto.vieira@outlook.com>
2022-04-29 22:20:36 -0300

    Small changes

--- public/main.js
+++ public/main.js
@@ -23,12 +23,10 @@ if (window.screen.availWidth <= 450) {
     userListHTML.classList.remove("hide");
 }
 
-setTimeout(function () {
-    let viewheight = $(window).height();
-    let viewwidth = $(window).width();
-    let viewport = document.querySelector("meta[name=viewport]");
-    viewport.setAttribute("content", "height=" + viewheight + "px, width=" + viewwidth + "px, initial-scale=1.0");
-}, 300);
+addEventListener("load", function() {
+    var viewport = document.querySelector("meta[name=viewport]");
+    viewport.setAttribute("content", viewport.content + ", height=" + window.innerHeight);
+})
 
 function renderUserList() {
     userListHTML.innerHTML = 'Usuários conectados<hr>';
